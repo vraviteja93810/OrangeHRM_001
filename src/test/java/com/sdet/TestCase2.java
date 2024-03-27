923255b0f7f8f939de26a74e9b84e480e2864baa
“raviteja” <raviteja2094@gmail.com>
2024-03-27 12:09:40 +0530

    Added TestCase3.java file and Modified Testcase2.java file

--- src/test/java/com/sdet/TestCase2.java
+++ src/test/java/com/sdet/TestCase2.java
@@ -1,2 +1,6 @@
 //This is test case2 which i have created.
 // this is newly added file.
+
+
+//New line added from workspace side.
+//This is newly added file.
---
+++ src/test/java/com/sdet/TestCase3.java
@@ -0,0 +1,7 @@
+package com.sdet;
+
+public class TestCase3 {
+public static void main(String[] args) {
+	System.out.println("New testcase added by me");
+}
+}
